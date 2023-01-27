bb95ed07f10d50842abbf484d859c3ab8746ab16
Bugaienko <sergiibugaienko@gmail.com>
2023-01-27 13:18:39 +0100

    first commit

---
+++ src/main/java/org/example/crudProject/MainAppStart.java
@@ -0,0 +1,11 @@
+package org.example.crudProject;
+
+/**
+ * @author Sergii Bugaienko
+ */
+
+public class MainAppStart {
+    public static void main(String[] args) {
+        new CommandLine().exec();
+    }
+}
---
+++ src/main/java/org/example/crudProject/PositionEnum.java
@@ -0,0 +1,9 @@
+package org.example.crudProject;
+
+public enum PositionEnum {
+    BOSS,
+    ASSISTANT,
+    CAFE_MAKER,
+    FRONT_END_PROG,
+    BACK_END_PROG
+}
